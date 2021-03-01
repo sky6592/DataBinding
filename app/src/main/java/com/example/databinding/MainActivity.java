d69e3744141ebf1d5f5e582654fc0be1fcc6146b
ihaneul <789_skymert@naver.com>
2021-03-01 20:29:26 +0900

    sky-MainActivity

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -12,9 +12,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        //완료 왜 안될까..ddd
+        // 메인 엑티비티 -> 엑티비티 메인 바인딩
+        // 자동으로 완성된 엑티비티 메인 바인딩 클래스 인스턴스를 가져왔다.
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         User user = new User("First Name","Last Name");
         activityMainBinding.setUser(user);
