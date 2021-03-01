ab2f3c75c334c41bbb5d9ca65c3f45d9537cd1f5
ihaneul <789_skymert@naver.com>
2021-03-01 17:05:52 +0900

    data binding

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.databinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+
+import com.example.databinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        User user = new User("First Name","Last Name");
+        activityMainBinding.setUser(user);
     }
 }
---
+++ app/src/main/java/com/example/databinding/User.java
@@ -0,0 +1,20 @@
+package com.example.databinding;
+
+
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+}
+
