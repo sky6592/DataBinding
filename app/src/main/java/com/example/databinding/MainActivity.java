aa374cc2e84673e96a7b60b25ad7ac2032fc22f4
ihaneul <789_skymert@naver.com>
2021-03-01 17:36:33 +0900

    plsss

--- app/src/main/java/com/example/databinding/MainActivity.java
+++ app/src/main/java/com/example/databinding/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        setContentView(R.layout.activity_main);
 
-        //완료 왜 안될까..
+        //완료 왜 안될까..ddd
         ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         User user = new User("First Name","Last Name");
         activityMainBinding.setUser(user);
